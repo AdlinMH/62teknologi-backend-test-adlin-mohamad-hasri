45a849f5220bad16b5a89010f2d66695a728c19c
Adlin Mohamad Hasri <adlin@rushowl.sg>
2023-12-05 09:39:56 +0700

    feat: add business PUT

--- app/api/business/route.ts
+++ app/api/business/route.ts
@@ -4,17 +4,19 @@ import prisma from '../../../lib/prisma'
 import { NextResponse } from 'next/server'
 import { Business } from './_types'
 
-export async function GET(request: Request) {
-    const business_search = await prisma.BusinessSearch.findMany()
-    return NextResponse.json({ business_search: business_search }, { status: 200 })
-}
-
-
 export async function POST(request: Request) {
   const res: Business = await request.json()
 
   await prisma.BusinessSearch.create(res)
 
-  const business_search = await prisma.BusinessSearch.findMany()
-  return NextResponse.json({ business_search: business_search }, { status: 200 })
+  return NextResponse.json({ data: null, success: true }, { status: 200 })
+}
+
+export async function PUT(request: Request) {
+  const res: Business = await request.json()
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: {res},
+  })
+  return NextResponse.json({ data: null, success: true }, { status: 200 })
 }
