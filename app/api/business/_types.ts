b4bc73fbaa5c7d268efbcf27af57b667a06fab71
Adlin Mohamad Hasri <adlin@rushowl.sg>
2023-12-05 09:31:12 +0700

    chore: add types

---
+++ app/api/business/_types.ts
@@ -0,0 +1,130 @@
+/**
+ * GET: business get search
+ * */
+export type BusinessesGetSearchReq = {
+  location?: string
+  latitude?: number
+  longitude?: number
+  term?: string
+  radius?: number
+  categories?: string[]
+  price?: number[]
+  open_at?: number
+  attributes?: string[]
+  sort_by?: string
+  device_platform?: string
+  reservation_date?: string
+  reservation_time?: string
+  reservation_covers?: number
+  matches_party_size_param?: boolean
+  limit?: number
+  offset?: number
+}
+export type BusinessesGetSearchRes = {
+  businesses: Business[]
+  total: number
+  region: {
+    center: {
+      latitude: string
+      longitude: string
+    }
+  }
+}
+
+
+/**
+ * GET: business get detail
+ * */
+export type BusinessesGetDetailParams = {
+  business_id_or_alias: number | undefined
+}
+export type BusinessesGetDetailRes = Omit<Business, 'distance' | 'attributes'> & {
+  is_claimed: boolean
+  photos: string[]
+}
+
+
+/**
+ * GET: business get review
+ * */
+export type BusinessesGetReviewParams = {
+  business_id_or_alias: number | undefined
+}
+export type BusinessesGetReviewReq = {
+  locale?: string
+  offset?: number
+  limit?: number
+  sort_by?: string
+}
+export type BusinessesGetReviewRes = {
+  total: number
+  reviews: Business_Review[]
+  possible_languages: string[]
+}
+
+
+/******* HELPERS /*******/
+export type Business = {
+  id: string
+  alias: string
+  name: string
+  image_url?: string
+  is_closed?: boolean
+  url?: string
+  review_count?: number
+  categories?: Business_Category[]
+  rating?: number
+  coordinates?: Business_Coordinate
+  transactions?: string[]
+  price?: string
+  location: Business_Location
+  phone: string
+  display_phone: string
+  distance?: string
+  hours?: Business_Hour[]
+  attributes?: ('liked_by_vegetarians' | 'liked_by_vegans' | 'hot_and_new')[]
+}
+export type Business_Category = {
+  alias: string
+  title: string
+}
+export type Business_Coordinate = {
+  latitude: number
+  longitude: number
+}
+export type Business_Location = {
+  address1: string
+  address2: string
+  address3: string
+  city: string
+  zip_code: string
+  country: string
+  state: string
+  display_address: string[]
+  cross_streets: string | null
+}
+export type Business_Hour = {
+  hour_type: string
+  open: Business_Hour_Open[]
+  is_open_now: boolean
+}
+export type Business_Hour_Open = {
+  day: number
+  start: string
+  end: string
+  is_overnight: boolean
+}
+export type Business_Review = {
+id: string
+url: string
+text: string
+rating: number
+time_created: string
+user: Business_Review_User
+}
+export type Business_Review_User = {
+  id: string
+  profile_url: string
+  image_url: string | null
+  name: string
+}
